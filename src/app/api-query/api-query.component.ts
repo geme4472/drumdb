49dd23f7bb6c278b187d2ceb654b203d9f79e1c8
Greg Ervin <geme4472@yahoo.com>
2020-12-12 17:38:46 -0600

    321

---
+++ src/app/api-query/api-query.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-api-query',
+  templateUrl: './api-query.component.html',
+  styleUrls: ['./api-query.component.css']
+})
+export class ApiQueryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { TopBarComponent } from "./top-bar/top-bar.component";
+
+import { HttpClientModule } from "@angular/common/http";
+import { MenuComponent } from './menu/menu.component';
+import { ApiQueryComponent } from './api-query/api-query.component';
+import { ApiFormComponent } from './api-form/api-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: "", component: ApiQueryComponent },
+      { path: "query/:queryOp", component: ApiQueryComponent },
+      { path: "form/:formID", component: ApiFormComponent }
+    ])
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    MenuComponent,
+    ApiQueryComponent,
+    ApiFormComponent
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
